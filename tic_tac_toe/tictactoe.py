8a3cf6124592eadd0827522d0b3a3a5c6fecc32c
aai-siraj <sm@adaptiveai.com>
2023-03-09 11:31:01 +0530

    Started updating main class

--- tic_tac_toe/tictactoe.py
+++ tic_tac_toe/tictactoe.py
@@ -9,3 +9,22 @@ if __name__ == "__main__":
     # Start the game loop
     while True:
         round = 1
+        #Display the Game introduction
+        intro = IntroDisplay()
+        intro.show_game_intro()
+        intro.explain_game_rules()
+
+        #Creating and initiliazing players and recording their data
+        players = Players()
+        player_manager = PlayerManager(players)
+        player_manager.get_player_names()
+        player_manager.get_player_colors()
+        player_manager.greet_players()
+        player_manager.initialize_player_scores()
+
+        while True:
+            game = Game()
+            game_option = GameOptionManager(player_manager)
+            
+
+    
